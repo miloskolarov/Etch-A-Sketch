bd77ae5403f9fb72c111465568a352a1e83eef08
miloskolarov <milos.v.kolarov@gmail.com>
2022-11-11 11:47:18 +0100

    Add functionality to 'sketch' and 'erase'buttons

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let color= "black"
+
 function createBoard (size) {
     let board= document.querySelector (".board");
     board.style.gridTemplateColumns= `repeat(${size}, 1fr)`;
@@ -13,23 +15,27 @@ function createBoard (size) {
 }
 
 function colorPixel () {
-    this.style.backgroundColor= "black";
+    this.style.backgroundColor= color;
 }
 
-//Add error message if input is too large or too small
-    //create else if in boardSize function
-        //if input is smaller than two, or larger than 100 print error
-            //Print error inside a paragraph
 
 function boardSize (input) {
     if (input >= 2 && input <= 100) {createBoard (input);
     } else if (input <2  || input > 100) { 
             let boardsz= document.querySelector(".boardsz");
             let error= document.createElement("p");
-            error.textContent= "Input must be between 2 and 100";
+            error.textContent= "Value must be between 2 and 100";
             error.style.color="red";
             boardsz.insertAdjacentElement("beforeend", error);}
            
+}
+
+// Creat colorChange Function
+// add white to errasor
+// add black to sketch
+// link with buttons
+function colorChange (choice) {
+    color= choice;
 }
 
 createBoard (16);
