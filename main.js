1fe9b4d9e0a48ac396afee2b271455b3ad4a0dae
miloskolarov <milos.v.kolarov@gmail.com>
2022-11-11 10:00:39 +0100

    Rename js file

---
+++ main.js
@@ -0,0 +1,7 @@
+//select board
+    //Set board grid
+    //populate board with divs
+        //make for loop
+let board= document.querySelector (".board");
+board.style.gridTemplateColumns= "repeat(16, 1fr)";
+board.style.gridTemplateRows= "repeat(16, 1fr)";
