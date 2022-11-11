dad48ab2c442eab53c3bf3bc494fc66c28d0d4de
miloskolarov <milos.v.kolarov@gmail.com>
2022-11-11 10:20:04 +0100

    Populate board with pixels

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-//select board
-    //Set board grid
-    //populate board with divs
-        //make for loop
 let board= document.querySelector (".board");
 board.style.gridTemplateColumns= "repeat(16, 1fr)";
 board.style.gridTemplateRows= "repeat(16, 1fr)";
+
+for (let i=0; i< 256; i++) {
+    let pixel= document.createElement("div");
+    pixel.style.backgroundColor= "black";
+    board.insertAdjacentElement("beforeend", pixel);
+}
