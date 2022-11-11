4c65d307316bdda205d759f251d6b5a55be10fb2
miloskolarov <milos.v.kolarov@gmail.com>
2022-11-11 10:33:41 +0100

    Color pixel on mouseover

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ board.style.gridTemplateRows= "repeat(16, 1fr)";
 
 for (let i=0; i< 256; i++) {
     let pixel= document.createElement("div");
-    pixel.style.backgroundColor= "black";
+    pixel.addEventListener("mouseover", colorPixel);
     board.insertAdjacentElement("beforeend", pixel);
 }
 
@@ -12,3 +12,7 @@ for (let i=0; i< 256; i++) {
         //make changeColor function
         //Call makeColor funcition inside eventListener method
         //Make placeholder variable containing color
+
+function colorPixel () {
+    this.style.backgroundColor= "black";
+}
